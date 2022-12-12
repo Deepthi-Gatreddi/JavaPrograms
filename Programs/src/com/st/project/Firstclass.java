9679c1a860d7cca4165f9c5be4a4b74920cfc192
DeepthiGatreddi <s180264@rguktsklm.ac.in>
2022-12-13 00:06:51 +0530

    commit

---
+++ Programs/src/com/st/project/Firstclass.java
@@ -0,0 +1,9 @@
+package com.st.project;
+
+public class Firstclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    System.out.println("welcome to my programming world...");
+	}
+}
