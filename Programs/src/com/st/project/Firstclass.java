8cb89abd795808254b283f1cd9ee1dd4f3c3e193
Deepthi-Gatreddi <deepthi180264@gmail.com>
2022-12-13 00:27:04 +0530

    first commit

--- Programs/src/com/st/project/Firstclass.java
+++ Programs/src/com/st/project/Firstclass.java
@@ -1,9 +1,15 @@
 package com.st.project;
 
 public class Firstclass {
+	public Firstclass() {
+		System.out.println("This is a constructor..."); //This is automatically called when a object of this class is created
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	    System.out.println("welcome to my programming world...");
+	    Firstclass obj=new Firstclass();
+	    
 	}
+
 }
